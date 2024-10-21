a0fb7700a8e60e322146cf96f9a7c93e332de0bb
jorge-sa <lucassairmaos@gmail.com>
2024-10-21 13:12:58 -0300

    fix PetProfile link

--- src/components/PetProfile.tsx
+++ src/components/PetProfile.tsx
@@ -5,13 +5,14 @@ import Link from "next/link"
 
 export default function PetProfile({id,name,description,imageUrl}:Pet) {
     return(
-        <div>
-          <img src={imageUrl} alt={description} />
+      <Link href={`/adoption/${id}`}>
+        <div className="h-[40%] w-[40%]">
+          <img src={imageUrl} alt={description} className="w-10 h-10"/>
           <div>
             <h2>{name}</h2>
             <p>{description}</p>
           </div>
-          <Link href={`/adoption/${id}`}/>
         </div>
+      </Link>
     )
 }
